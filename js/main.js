e48da6c8e7aba30cbd27a2f947686311916d7c81
Piotr Leoszewski <p.leoszewski@gmail.com>
2017-12-17 14:01:29 +0100

    Update js in team section.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-var members = document.querySelector('.members');
+var members = document.querySelectorAll('.members');
 
-members.addEventListener('mouseenter', function () {
-    querySelector('.members-picture').style.transform = 'translateY(-10%) scale(0.7)';
-    querySelector('.members-picture').style.transition = '0.4s';
+members.forEach(function (value) {
+    value.addEventListener('mouseenter', function (e) {
+        let member = e.target;
+        member.querySelector('.members-picture').style.transform = 'translateY(-10%) scale(0.7)';
+        // console.log(member.querySelector('.members-picture'));
+    });
+
 });
 
-members.addEventListener('mouseover', function () {
-    querySelector('.social-media').style.visibility = 'visible';
-    querySelector('.social-media').style.transition-delay = '0.3s';
-});
+
+console.log(members);
+
+
+// members.addEventListener('mouseover', function () {
+//     querySelector('.social-media').style.visibility = 'visible';
+//     querySelector('.social-media').style.transition-delay = '0.3s';
+// });
+
+
